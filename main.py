315a22340a9dd56c7df2b585a5dd1ec0c913e717
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2026-05-13 13:03:20 +0300

    feat: Models & templates

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ print("Select action:")
 print("1. Log in into account.")
 print("2. Register new account.")
 print("3. Exit from program.")
-
--- models/models.py
+++ models/models.py
@@ -1,12 +1,22 @@
 from datetime import date
 
 class EvenetHall:
-    def __init__(self, capacity: int, rows: int):
+    def __init__(self, capacity: int, rows: int, created_on: date):
         self.capacity = capacity
         self.rows = rows
+        self.created_on = created_on
+
+    def __str__(self):
+        return f"Event hall created on {str(self.created_on)} with capacity of {self.capacity}."
+        
 
 class Ticket:
-    def __init__(self, price: float | int, date: date, person: str):
+    def __init__(self, price: float | int, date: date, person: str, seat: None | str, row: None | str):
         self.price = price
         self.date = date
         self.person = person
+        self.row = row
+        self.seat = seat
+
+    def __str__(self):
+        return f"Ticket for {self.person} for {self.date}."
